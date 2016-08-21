152e27963dd6384ef24363d5069c26c26fa2f450
amitlondhe <alondhe@ICC11135.iccohio.com>
2016-08-20 20:08:57 -0400

    More changes
    Data moved to JS file.

---
+++ js/sankey-data.js
@@ -0,0 +1,50 @@
+var dataForChinese = [
+['US-NC', 3.22],
+['US-NV', 3.29],
+['US-AZ', 3.32],
+['US-IL', 3.41],
+['US-PA', 3.28],
+['US-WI', 3.34],
+['US-SC', 3.73]
+]
+
+
+var dataForRestaurants = [
+['US-NC',3.41],
+['US-TX', 4.0],
+['US-NM', 3.0],
+['US-NV',3.41],
+['US-AZ',3.42],
+['US-IL',3.35],
+['US-PA',3.56],
+['US-WI',3.45],
+['US-SC',3.34]
+]
+
+var dataForFastFood = [
+['US- NC', 3.04],
+['US- NV', 2.89],
+['US- AZ', 2.92],
+['US- IL', 2.68],
+['US- PA', 2.89],
+['US- WI', 3.02],
+['US- SC', 2.94]
+]
+
+var usaz = [["fast food", "US-AZ", 2.92],["restaurants", "US-AZ", 3.42],["chinese","US-AZ", 3.32], ["US-AZ", "percentasian", 2.76], ["US-AZ", "percentblack", 4.05], ["US-AZ", "percentwhite", 73.01]]
+
+var usil = [["fast food","US-IL", 2.68],["restaurants","US-IL", 3.35],["chinese","US-IL", 3.41], ["US-IL", "percentasian", 4.57], ["US-IL", "percentblack", 14.55], ["US-IL", "percentwhite", 71.53]]
+
+var usnc = [["fast food","US-NC", 3.04],["restaurants","US-NC", , 3.41],[ "chinese","US-NC", 3.22], ["US-NC", "percentasian", 2.19], ["US-NC", "percentblack", 21.48], ["US-NC", "percentwhite", 68.47]]
+
+var usnm = [["restaurants","US-NM", 3.0], ["US-NM", "percentasian", 1.37], ["US-NM", "percentblack", 2.07], ["US-NM", "percentwhite", 68.37]]
+
+var usnv = [["fast food","US-NV", 2.89],["restaurants","US-NV", 3.41],["chinese","US-NV", 3.29], ["US-NV", "percentasian", 7.24], ["US-NV", "percentblack", 8.10], ["US-NV", "percentwhite", 66.16]]
+
+var uspa = [["fast food","US-PA", 2.89],["restaurants","US-PA", 3.56],[ "chinese","US-PA", 3.28], ["US-PA", "percentasian", 2.75], ["US-PA", "percentblack", 10.85], ["US-PA", "percentwhite", 81.92]]
+
+var ussc = [["fast food","US-SC",  2.94],["restaurants","US-SC", 3.34],["chinese","US-SC", 3.73], ["US-SC", "percentasian", 1.28], ["US-SC", "percentblack", 27.90], ["US-SC", "percentwhite", 66.16]]
+
+var ustx =  [["restaurants","US-TX", 4.0], ["US-TX", "percentasian", 3.84], ["US-TX", "percentblack", 11.85], ["US-TX", "percentwhite", 70.40]]
+
+var uswi = [["fast food","US-WI", 3.02],[ "restaurants","US-WI", 3.45],["chinese","US-WI", 3.34], ["US-WI", "percentasian", 2.27], ["US-WI", "percentblack", 6.32], ["US-WI", "percentwhite", 86.20]]
